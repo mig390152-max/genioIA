a6fb6785332547ec9d18d7b986f4378536edc1c7
Miguel <mig390152@gmail.com>
2026-05-24 14:08:07 -0300

    api ia

--- api/chat.js
+++ api/chat.js
@@ -1,25 +1,33 @@
 export default async function handler(req, res) {
+
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método não permitido" });
+    return res.status(405).json({
+      error: "Método não permitido"
+    });
   }
 
   try {
+
     const apiKey = process.env.GROQ_API_KEY;
 
     if (!apiKey) {
-      return res.status(500).json({ error: "API KEY não encontrada" });
+      return res.status(500).json({
+        error: "API KEY não encontrada"
+      });
     }
 
     const resposta = await fetch(
       "https://api.groq.com/openai/v1/chat/completions",
       {
         method: "POST",
+
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${apiKey}`,
         },
+
         body: JSON.stringify({
-          model: "llama3-70b-8192",
+          model: "llama-3.3-70b-versatile",
           messages: req.body.messages,
           temperature: 0.7
         })
@@ -31,7 +39,11 @@ export default async function handler(req, res) {
     return res.status(200).json(dados);
 
   } catch (erro) {
+
     console.log(erro);
-    return res.status(500).json({ error: "Erro interno na IA" });
+
+    return res.status(500).json({
+      error: "Erro interno na IA"
+    });
   }
 }
